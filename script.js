e17a996b35a11b325a9b10d8c94a1d9f39ce9d2e
vinithasagar <159407761+vinithasagar@users.noreply.github.com>
2024-09-16 22:35:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,264 @@
+function vred(){
+    document.getElementById("text_body").style.backgroundColor="red";
+}
+function volive(){
+    document.getElementById("text_body").style.backgroundColor="#808000";
+}
+function vaquamarine(){
+    document.getElementById("text_body").style.backgroundColor="#7FFFD4";
+}
+function vSeablue(){
+    document.getElementById("text_body").style.backgroundColor="#C2DFFF";
+}
+function vlavenderblue(){
+    document.getElementById("text_body").style.backgroundColor="#E3E4FA";
+}
+function vvanilla(){
+    document.getElementById("text_body").style.backgroundColor="#F3E5AB";
+}
+function vredfox(){
+    document.getElementById("text_body").style.backgroundColor="#C35817";
+}
+function vfrelilac(){
+    document.getElementById("text_body").style.backgroundColor="#86608E";
+}
+function voldrose(){
+    document.getElementById("text_body").style.backgroundColor="#C08081";
+}
+function vpastelorange(){
+    document.getElementById("text_body").style.backgroundColor="#F8B88B";
+}
+function vhelipurple(){
+    document.getElementById("text_body").style.backgroundColor="#D462FF";
+}
+function vcyan(){
+    document.getElementById("text_body").style.backgroundColor="#00FFFF";
+}
+function vblue(){
+    document.getElementById("text_body").style.backgroundColor="#0000FF";
+}
+function vdarkblue(){
+    document.getElementById("text_body").style.backgroundColor="#00008B";
+}
+function vlightblue(){
+    document.getElementById("text_body").style.backgroundColor="#ADD8E6";
+}
+function vpurple(){
+    document.getElementById("text_body").style.backgroundColor="#800080";
+}
+function vyellow(){
+    document.getElementById("text_body").style.backgroundColor="#FFFF00";
+}
+function vlime(){
+    document.getElementById("text_body").style.backgroundColor="#00FF00";
+}
+function vmagenta(){
+    document.getElementById("text_body").style.backgroundColor="#FF00FF";
+}
+function vpink(){
+    document.getElementById("text_body").style.backgroundColor="#FFC0CB";
+}
+function vsilver(){
+    document.getElementById("text_body").style.backgroundColor="#C0C0C0";
+}
+function vgray(){
+    document.getElementById("text_body").style.backgroundColor="#808080";
+}
+function vorange(){
+    document.getElementById("text_body").style.backgroundColor="#FFA500";
+}
+function vbrown(){
+    document.getElementById("text_body").style.backgroundColor="#A52A2A";
+}
+function vmaroon(){
+    document.getElementById("text_body").style.backgroundColor="#800000";
+}
+function vgreen(){
+    document.getElementById("text_body").style.backgroundColor="#008000";
+}
+
+function vdwhite(){
+    document.getElementById("text_body").style.backgroundColor="#E1D9D1";
+}
+function vpowderpink(){
+    document.getElementById("text_body").style.backgroundColor="#FFB2D0";
+}
+function vdarkscar(){
+    document.getElementById("text_body").style.backgroundColor="#560319";
+}
+function vpastelbrown(){
+    document.getElementById("text_body").style.backgroundColor="#B1907F";
+}
+function vsaladgreen(){
+    document.getElementById("text_body").style.backgroundColor="#A1C935";
+}
+function vfroggreen(){
+    document.getElementById("text_body").style.backgroundColor="#99C68E";
+}
+function vreset(){
+    document.getElementById("text_body").style.backgroundColor="white";
+}
+
+
+function myred(){
+    document.getElementById("output").style.color="red";
+}
+function mycyan(){
+    document.getElementById("output").style.color="#00FFFF";
+}
+function myblue(){
+    document.getElementById("output").style.color="#0000FF";
+}
+function mydarkblue(){
+    document.getElementById("output").style.color="#00008B";
+}
+function mylightblue(){
+    document.getElementById("output").style.color="#ADD8E6";
+}
+function mypurple(){
+    document.getElementById("output").style.color="#800080";
+}
+function myyellow(){
+    document.getElementById("output").style.color="#FFFF00";
+}
+function mylime(){
+    document.getElementById("output").style.color="#00FF00";
+}
+function mymagenta(){
+    document.getElementById("output").style.color="#FF00FF";
+}
+function mypink(){
+    document.getElementById("output").style.color="#FFC0CB";
+}
+function mysilver(){
+    document.getElementById("output").style.color="#C0C0C0";
+}
+function mygray(){
+    document.getElementById("output").style.color="#808080";
+}
+function myorange(){
+    document.getElementById("output").style.color="#FFA500";
+}
+function mybrown(){
+    document.getElementById("output").style.color="#A52A2A";
+}
+function mymaroon(){
+    document.getElementById("output").style.color="#800000";
+}
+function mygreen(){
+    document.getElementById("output").style.color="#008000";
+}
+function myolive(){
+    document.getElementById("output").style.color="#808000";
+}
+function myaquamarine(){
+    document.getElementById("output").style.color="#7FFFD4";
+}
+function mySeablue(){
+    document.getElementById("output").style.color="#C2DFFF";
+}
+function mylavenderblue(){
+    document.getElementById("output").style.color="#E3E4FA";
+}
+function myvanilla(){
+    document.getElementById("output").style.color="#F3E5AB";
+}
+function myredfox(){
+    document.getElementById("output").style.color="#C35817";
+}
+function myfrelilac(){
+    document.getElementById("output").style.color="#86608E";
+}
+function myoldrose(){
+    document.getElementById("output").style.color="#C08081";
+}
+function mypastelorange(){
+    document.getElementById("output").style.color="#F8B88B";
+}
+function myhelipurple(){
+    document.getElementById("output").style.color="#D462FF";
+}
+function mydwhite(){
+    document.getElementById("output").style.color="#E1D9D1";
+}
+function mypowderpink(){
+    document.getElementById("output").style.color="#FFB2D0";
+}
+function mydarkscar(){
+    document.getElementById("output").style.color="#560319";
+}
+function mypastelbrown(){
+    document.getElementById("output").style.color="#B1907F";
+}
+function mysaladgreen(){
+    document.getElementById("output").style.color="#A1C935";
+}
+function myfroggreen(){
+    document.getElementById("output").style.color="#99C68E";
+}
+function myreset(){
+    document.getElementById("output").style.color="#000"
+}
+let textarea = document.getElementById("output");
+let fontSize = 10;
+
+function myfont_1(){
+    document.getElementById("output").style.fontFamily="'Courier New', Courier, monospace";
+}
+function myfont_2(){
+    document.getElementById("output").style.fontFamily="'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif";
+}
+function myfont_3(){
+    document.getElementById("output").style.fontFamily="'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif";
+}
+function myfont_4(){
+    document.getElementById("output").style.fontFamily="Brush Script MT";
+}
+function myfont_5(){
+    document.getElementById("output").style.fontFamily="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif";
+}
+function myfont_6(){
+    document.getElementById("output").style.fontFamily="'Times New Roman', Times, serif";
+}
+function myfont_7(){
+    document.getElementById("output").style.fontFamily="'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif";
+}
+function myfont_8(){
+    document.getElementById("output").style.fontFamily="Algerian";
+}
+function myfont_9(){
+    document.getElementById("output").style.fontFamily="Cambria, Cochin, Georgia, Times, 'Times New Roman', serif";
+}
+function myfont_10(){
+    document.getElementById("output").style.fontFamily="Georgia, 'Times New Roman', Times, serif";
+}
+function myfont_11(){
+    document.getElementById("output").style.fontFamily=" Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif";
+}
+function myfont_12(){
+    document.getElementById("output").style.fontFamily="Verdana, Geneva, Tahoma, sans-serif";
+}
+function myfont_13(){
+    document.getElementById("output").style.fontFamily="cursive";
+}
+function myfont_14(){
+    document.getElementById("output").style.fontFamily="Old English Text MT";
+}
+function myfont_15(){
+    document.getElementById("output").style.fontFamily="sans-serif";
+}
+function myfont_16(){
+    document.getElementById("output").style.fontFamily="jokerman";
+}
+function myfont_17(){
+    document.getElementById("output").style.fontFamily="Papyrus";
+}
+function myfont_18(){
+    document.getElementById("output").style.fontFamily="Chiller";
+}
+function myfont_19(){
+    document.getElementById("output").style.fontFamily="Curlz MT";
+}
+function myreset2(){
+    document.getElementById("output").style.fontFamily="system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif";
+}
